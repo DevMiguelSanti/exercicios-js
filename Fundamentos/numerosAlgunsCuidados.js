85352236ca5ce5ffbc1f98c118ea0a3a12bb1508
Miguel Santi <miguelsanti_06@hotmail.com>
2022-06-27 23:26:06 -0300

    add templastes strings

--- Fundamentos/numerosAlgunsCuidados.js
+++ Fundamentos/numerosAlgunsCuidados.js
@@ -4,3 +4,5 @@ console.log("Show!" *2)
 console.log((0.1 + 0.7).toFixed(2))
 //console.log(10.toString())
 console.log((10.345).toFixed(2))
+console.log('3' + 2) //String com "+" concatena. Com "-" subtrai
+console.log('3' - 2) //String com "+" concatena. Com "-" subtrai
